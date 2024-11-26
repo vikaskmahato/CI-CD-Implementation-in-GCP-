72726b48f13cc0b69d025f46d1f4f49a74be3589
Vikas Mahato <51792385+vikaskmahato@users.noreply.github.com>
2024-11-26 22:19:26 +0530

    Update view_script.sql

--- sql/view_script.sql
+++ sql/view_script.sql
@@ -1,1 +1,5 @@
-SELECT 1;
+CREATE OR REPLACE VIEW `airy-parsec-400006.exoplanet_dataset.test_view` AS
+(
+  SELECT pl_name, hostname
+  FROM `airy-parsec-400006.exoplanet_dataset.planetary_systems`
+);
