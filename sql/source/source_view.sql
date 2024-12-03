74c873b82f1440475d73270de3e73a408b8f052f
Vikas Mahato <51792385+vikaskmahato@users.noreply.github.com>
2024-12-03 20:03:49 +0530

    Update source_view.sql

--- sql/source/source_view.sql
+++ sql/source/source_view.sql
@@ -2,5 +2,4 @@ CREATE OR REPLACE VIEW `airy-parsec-400006.exoplanet_dataset.source_view` AS
 (
   SELECT pl_name, hostname
   FROM `airy-parsec-400006.exoplanet_dataset.planetary_systems`
-)
-  ;
+);
