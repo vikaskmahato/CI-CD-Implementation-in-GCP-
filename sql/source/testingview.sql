300543b6949d537e71bafcc0429a57da1ca50887
Vikas_GCP <vikasgcp9@gmail.com>
2024-12-06 07:21:14 +0000

    Testing push from google cloud

--- sql/source/testingview.sql
+++ sql/source/testingview.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE VIEW `airy-parsec-400006.exoplanet_dataset.testview` AS
 (
   SELECT pl_name, hostname, pl_orbper FROM `airy-parsec-400006.exoplanet_dataset.planetary_systems`
-);
+)
+;
