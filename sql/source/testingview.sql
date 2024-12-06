2494e4a058cdc5e324a9ade9ba6220af92faa578
Vikas_GCP <vikasgcp9@gmail.com>
2024-12-06 07:50:24 +0000

    Updated testview code

--- sql/source/testingview.sql
+++ sql/source/testingview.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE VIEW `airy-parsec-400006.exoplanet_dataset.testview` AS
 (
   SELECT pl_name, hostname, pl_orbper FROM `airy-parsec-400006.exoplanet_dataset.planetary_systems`
-)
-;
+);
